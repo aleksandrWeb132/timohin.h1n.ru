88402ee16fee8c188d7fc460558ecb7b7289a251
Александр Тимохин <a.timohin.prog@solo-it.ru>
2024-11-29 16:47:23 +0400

    Реализовал передачу подключения к бд в класс и название таблици.

--- lib/Orm.php
+++ lib/Orm.php
@@ -3,5 +3,11 @@
 namespace lib;
 
 class Orm {
+    private $connect;
+    private $table;
 
+    function __construct($connect, $table) {
+        $this->connect = $connect;
+        $this->table = $table;
+    }
 }
