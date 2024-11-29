56995b29b70465119fdac3763802a2372d92893f
Александр Тимохин <a.timohin.prog@solo-it.ru>
2024-11-29 16:48:46 +0400

    Реализовал приватный метод для собирания ответов от БД

--- lib/Orm.php
+++ lib/Orm.php
@@ -10,4 +10,17 @@ class Orm {
         $this->connect = $connect;
         $this->table = $table;
     }
+
+
+    private function getArray($result) {
+        $data = [];
+
+        if($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                $data[] = $row;
+            }
+        }
+
+        return $data;
+    }
 }
