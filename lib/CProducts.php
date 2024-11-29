3f7622733d49843b80f98d86b2e62613901b3b76
Александр Тимохин <a.timohin.prog@solo-it.ru>
2024-11-29 17:53:50 +0400

    Реализовал функцию которая обновляет значение Quantity.

--- lib/CProducts.php
+++ lib/CProducts.php
@@ -7,4 +7,24 @@ class CProducts extends Orm {
         parent::__construct($connect, "Products");
     }
 
+    public function updateQuantity($idProduct, $value) {
+        $idList = $this->findId("PRODUCT_ID", $idProduct);
+
+        if(count($idList) === 0) {
+            return [
+                "code" => 3442,
+                "message" => "Ошибка, не удалось изменить количество продуктов!"
+            ];
+        }
+
+        $this->update($idList[0]["ID"], [
+            "PRODUCT_QUANTITY" => $value
+        ]);
+
+        return [
+            "code" => 1,
+            "message" => "Количество продуктов изменилось!"
+        ];
+    }
+
 }
