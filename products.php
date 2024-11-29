068e2e8df50d87124dda5c3c3835ce848fc6af46
Александр Тимохин <a.timohin.prog@solo-it.ru>
2024-11-29 17:05:19 +0400

    Реализовал файдл для получения запросов от front-end

---
+++ products.php
@@ -0,0 +1,27 @@
+<?php
+require_once "./config/config.php";
+require_once "./lib/Orm.php";
+require_once "./lib/CProducts.php";
+
+use lib\CProducts;
+
+$accessBd = getAccessBd();
+
+$host = $accessBd["host"];
+$username = $accessBd["username"];
+$password = $accessBd["password"];
+$database = $accessBd["database"];
+
+$connect = new mysqli($host, $username, $password, $database);
+
+if($connect->connect_error) {
+    die(json_encode([
+        "code" => 40401,
+        "message" => "Ошибка, не удалось подключиться к БД!"
+    ]));
+}
+
+$products = new CProducts($connect, "Products");
+
+
+$connect->close();
