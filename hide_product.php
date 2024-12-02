dd874453709300da761695778bbf9d35fe78fb29
Александр Тимохин <a.timohin.prog@solo-it.ru>
2024-12-02 12:56:03 +0400

    Создал страницу для получения запроса на скрытие продукта.

---
+++ hide_product.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once "./config/config.php";
+require_once "./lib/Orm.php";
+require_once "./lib/CProducts.php";
+
+use lib\CProducts;
+
+// Убедитесь, что заголовок указывает, что вы ожидаете JSON
+header("Content-Type: application/json");
+
+// Получаем данные из тела запроса
+$data = json_decode(file_get_contents("php://input"), true);
+
+if(!isset($data["productId"])) {
+    die(json_encode([
+        "code" => 404092,
+        "message" => "Ошибка, не переданы параметры!"
+    ]));
+}
+
+if(!is_numeric($data["productId"])) {
+    die(json_encode([
+        "code" => 404082,
+        "message" => "Ошибка, переданные параметры должны быть числом!"
+    ]));
+}
+
+$productId = htmlspecialchars($data["productId"], ENT_QUOTES, 'UTF-8');
+$productQuantity = htmlspecialchars($data["productQuantity"], ENT_QUOTES, 'UTF-8');
+
+$accessBd = getAccessBd();
+
+$host = $accessBd["host"];
+$username = $accessBd["username"];
+$password = $accessBd["password"];
+$database = $accessBd["database"];
+
+$connect = new mysqli($host, $username, $password, $database);
+
+if($connect->connect_error) {
+    die(json_encode([
+        "code" => 40401,
+        "message" => "Ошибка, не удалось подключиться к БД!"
+    ]));
+}
+
+$products = new CProducts($connect);
+
+$hide = $products->hideLine($productId);
+
+$connect->close();
+
+die(json_encode($hide));
