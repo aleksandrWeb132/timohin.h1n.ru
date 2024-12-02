4fdca44468062aea32bc6cc9185dd7c41c78021a
Александр Тимохин <a.timohin.prog@solo-it.ru>
2024-12-02 12:16:20 +0400

    Реализовал отправту запроса для обновления количества продуктов.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,50 @@
+var timeout;
 
+function updateQuantity(id, quantity) {
+    clearTimeout(timeout);
 
+    timeout = setTimeout(() => {
+        const url = 'http://timohin.h1n.ru/update_product.php';
+        const data = {
+            productId: id,
+            productQuantity: quantity
+        };
 
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(response => {
+                if(!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
 
-
-
-
-
-
-
-
+    }, 1000);
+}
 
 function addQuantity(id) {
     const lineElement = document.getElementById(id);
     const quantityElement = lineElement.querySelector('.quantity__number');
 
-    quantityElement.textContent = String(Number(quantityElement.textContent) + 1);
+    let quantity = Number(quantityElement.textContent) + 1;
+
+    quantityElement.textContent = String(quantity);
+
+    updateQuantity(id, quantity);
 }
 
 function removeQuantity(id) {
     const lineElement = document.getElementById(id);
     const quantityElement = lineElement.querySelector('.quantity__number');
 
-    quantityElement.textContent = String(Number(quantityElement.textContent) - 1);
+    let quantity = Number(quantityElement.textContent) - 1;
+
+    quantityElement.textContent = String(quantity);
+
+    updateQuantity(id, quantity);
 }
